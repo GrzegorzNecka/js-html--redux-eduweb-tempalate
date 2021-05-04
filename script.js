b0fe3d2d92dad330d2bcf5bb74b5e8c7e22367c1
GrzegorzNecka <strzypior@gmail.com>
2021-05-04 13:05:32 +0200

    1 file modified

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ const initialState = {
   notes: []
 };
 
-const myReducer = (state = initialState, action) => {};
+const myReducer = (state = initialState, action) => {
+  console.log(action);
+};
+
+const noteAction = {
+  type: "ADD_NOTE",
+  payload: { title: "Hello", content: "lorem" }
+};
 
 const store = createStore(myReducer);
+
+store.dispatch();
+
+/**
+ *
+ * type - nazwa akcji
+ * paylod - zawartośc akcji
+ * dispatch() - służy do wysyłania akcji do reducera
+ *
+ */
